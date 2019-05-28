34438f004e0b45cbd095ce995cb8e73466145f02
apastoriza <anton@antonpastoriza.com>
2019-05-28 19:35:01 +0200

    skeleton deployable version

---
+++ k8s-xmetrics-agent/src/main/java/com/k8s/xmetrics/agent/controller/HelloController.java
@@ -0,0 +1,32 @@
+package com.k8s.xmetrics.agent.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author apastoriza
+ */
+@RestController
+public class HelloController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
+
+	@Autowired
+	private DiscoveryClient discoveryClient;
+
+	@RequestMapping("/")
+	public String hello() {
+		return "Hello World";
+	}
+
+	@RequestMapping("/services")
+	public List<String> services() {
+		return this.discoveryClient.getServices();
+	}
+
+}
---
+++ k8s-xmetrics-agent/src/test/java/com/k8s/xmetrics/test/agent/boot/ApplicationTestIT.java
@@ -0,0 +1,34 @@
+package com.k8s.xmetrics.test.agent.boot;
+
+import com.k8s.xmetrics.agent.boot.Application;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * @author apastoriza
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+public class ApplicationTestIT {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationTestIT.class);
+
+	@Autowired
+	private ApplicationContext context;
+
+	/*
+	 * This test proves that the application can be loaded successful and that
+	 * all @configurations and dependencies are there
+	 */
+	@Test
+	public void contextLoads() throws Exception {
+		assertThat(this.context).isNotNull();
+	}
+}
